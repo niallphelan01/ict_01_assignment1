f76464c5d6b0a015476d99972846277c5a954392
Niall Phelan <niall@medtec.ie>
2019-09-01 12:59:55 +0100

    removed unnecessary code in the assessmentlist.js file

--- controllers/assessmentlist.js
+++ controllers/assessmentlist.js
@@ -13,41 +13,7 @@ const assessmentlist = {
             assessmentlist: assessmnentlistStore.getAssessmentlist(assessmentlistId)
         };
         response.render("assessmentlist", viewData);
-    },
-
-   addAssessment(request, response){
-       const loggedInUser = accounts.getCurrentUser(request);
-       logger.debug("the logged in user" + loggedInUser);
-       const newAssessment = {
-           id:uuid(),
-           userid:loggedInUser.id,
-           assessments: []
-      //         id:uuid();
-      //     date: request.body.date,
-      //     weight: request.body.weight,
-      //     chest: request.body.chest,
-      //     thigh: request.body.thigh,
-      //     upperarm: request.body.upperarm,
-      //     waist: request.body.waist,
-      //     hips: request.body.hips
-       };
-       logger.debug("Creating a new Assessment", newAssessment);
-       assessmnentlistStore.addAssessment(newAssessment);
-       response.redirect('/dashboard');
-
-       // addPlaylist(request, response) {
-       //   const loggedInUser = accounts.getCurrentUser(request);
-       //   const newPlayList = {
-       //     id: uuid(),
-       //     userid: loggedInUser.id,
-       //     title: request.body.title,
-       //     songs: []
-       //   };
-       //   logger.debug("Creating a new Playlist", newPlayList);
-       //   playlistStore.addPlaylist(newPlayList);
-       //   response.redirect("/dashboard");
-       // }
-   }
-};
+    }
+   };
 
 module.exports = assessmentlist;
